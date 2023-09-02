a4be5fa5abc5364c1ffb18145d10c4b8d897f583
Jose Castro <daniel1819castro@gmail.com>
2023-09-01 23:35:01 -0400

    Segunda modificacion, colorView

--- src/Journal/pages/JournalPage.jsx
+++ src/Journal/pages/JournalPage.jsx
@@ -1,13 +1,14 @@
-
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView } from "../view"
+import { NothingSelectedView } from "../view/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       
-      <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis temporibus eveniet alias quas, magni a consequatur obcaecati cupiditate cum illum iusto voluptate perspiciatis vitae asperiores nam reprehenderit tempore perferendis aperiam.</Typography>
-
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis temporibus eveniet alias quas, magni a consequatur obcaecati cupiditate cum illum iusto voluptate perspiciatis vitae asperiores nam reprehenderit tempore perferendis aperiam.</Typography> */}
+      <NothingSelectedView />  
+      {/* <NoteView />  */}
 
 
     </JournalLayout>
