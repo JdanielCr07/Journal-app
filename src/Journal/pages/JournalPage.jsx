5366a18fec26998588efb08bee2925a2002318e0
Jose Castro <daniel1819castro@gmail.com>
2023-09-09 10:50:23 -0400

    Otro commit

--- src/Journal/pages/JournalPage.jsx
+++ src/Journal/pages/JournalPage.jsx
@@ -7,8 +7,8 @@ export const JournalPage = () => {
     <JournalLayout>
       
       {/* <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis temporibus eveniet alias quas, magni a consequatur obcaecati cupiditate cum illum iusto voluptate perspiciatis vitae asperiores nam reprehenderit tempore perferendis aperiam.</Typography> */}
-      <NothingSelectedView />  
-      {/* <NoteView />  */}
+        <NothingSelectedView />   
+      {/* <NoteView />   */}
 
 
     </JournalLayout>
