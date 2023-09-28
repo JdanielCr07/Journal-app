8b5f994c2d6f264b99f090c6e945af44fcf17c50
Jose Castro <daniel1819castro@gmail.com>
2023-09-27 23:29:49 -0400

    git commit

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,12 +1,37 @@
+import { useDispatch } from 'react-redux';
 import {Link as RouterLink} from 'react-router-dom';
 import { Google } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography, Link} from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+import { checkingAuthentication, startgGoogleSignIn } from '../../store/auth/thunks';
+
 
 export const LoginPage = () => {
+  
+  const dispatch = useDispatch() 
+
+  const { email, password, onInputChange} = useForm({
+    email: 'jose@google.com',
+    password: '123456'
+  });
+
+  const onSubmit = (event)=>{
+    event.preventDefault();
+
+    console.log({email, password})
+    dispatch( checkingAuthentication() );
+  }
+
+  const onGoogleSignIn = () =>{
+    console.log('onGoogleSingIn')
+    dispatch(startgGoogleSignIn())
+
+  }
+
   return (
     <AuthLayout title= 'Inicio de sesión'>
-      <form>
+      <form onSubmit={onSubmit}>
           <Grid container>
             <Grid item xs= { 12 } sx={{ mt : 2 }}>
               <TextField 
@@ -14,7 +39,9 @@ export const LoginPage = () => {
                 type="email" 
                 placeholder="correo@google.com" 
                 fullWidth
-                required
+                name='email'
+                value={email}
+                onChange={onInputChange}
               />           
             </Grid>
 
@@ -24,19 +51,21 @@ export const LoginPage = () => {
                 type="password" 
                 placeholder="contraseña" 
                 fullWidth
-                required
+                name='password'
+                value={password}
+                onChange={onInputChange}
               />
             </Grid>
             
             <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1}}>
               <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant="contained" fullWidth>
+                <Button type='submit' variant="contained" fullWidth>
                   Ingresar
                 </Button>
               </Grid>
 
               <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant="contained" fullWidth>
+                <Button onClick={ onGoogleSignIn } variant="contained" fullWidth>
                   <Google/>
                   <Typography sx={{ ml: 1 }}>Google</Typography>
                 </Button>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './Styles.css'
 import { Journalapp } from './Journal-app.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Journalapp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Journalapp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
